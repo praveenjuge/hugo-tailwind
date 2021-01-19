e45847812f1ae14083cc861dc83b3cd2a586e9c9
praveenjuge <praveen@skcript.com>
2021-01-19 14:30:22 +0530

    Update to latest versions
    Signed-off-by: praveenjuge <praveen@skcript.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,12 @@ module.exports = {
   purge: {
     content: ["./layouts/**/*.html", "./content/**/*.md", "./content/**/*.html"],
   },
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 };
